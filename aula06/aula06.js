0acdd56e980693f937705a666f0d3d5aae103f06
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-02-10 14:42:28 -0300

    Aula 6
    Dúvidas parte 1.

---
+++ aula06/aula06.js
@@ -0,0 +1,7 @@
+// "===" é diferente de "==", pois a comparação leva em consideração o ponteiro, tipo e o conteúdo da variável
+// Variáveis podem receber entrada de dados
+// Not é um operador lógico 
+// É possível verificar o tipo de dispositivo que está carregando a página
+// DOM controla todos os elementos da página
+// É possível declarar variáveis com os prefixos "g_", "l_" e "c_"
+// Let é mais leve que o var
