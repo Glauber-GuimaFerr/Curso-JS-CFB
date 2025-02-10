f7932b4e88b376ccab599649320cd561c89458c5
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-02-10 10:00:30 -0300

    Aula 4
    Operadores matemáticos.

---
+++ aula04/aula04.js
@@ -0,0 +1,31 @@
+/*
++
+-
+/
+*
+%
+++
+--
++=
+-=
+*=
+/=
+%=
+*/
+
+let num1 = 0, num2 = 0, res = 0
+
+num1 = 5
+num2 = 10
+res = (num1 + num2)*2
+res += 3
+res -= 2
+
+console.log(num1)
+console.log(num2)
+console.log(res)
+console.log(num2-num1)
+console.log(num2/num1)
+console.log((num2 + 1) % num1)
+console.log(++num1)
+console.log(--num2)
