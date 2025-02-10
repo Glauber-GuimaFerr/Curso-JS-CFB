f9ec30b4a16184b2b7c518d6896ef6894f01a41a
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-02-10 14:21:54 -0300

    Aula 5
    Operadores relacionais.

---
+++ aula05/aula05.js
@@ -0,0 +1,20 @@
+/*
+!
+>
+<
+>=
+<=
+==
+===
+!=
+*/
+
+let num1 = 10
+let num2 = 20
+let num3 = 5
+
+console.log(num1 > num2)
+console.log(num2 > num3)
+console.log(num1 >= num1)
+console.log(num1 != num3)
+console.log(!num1 == !num2)
