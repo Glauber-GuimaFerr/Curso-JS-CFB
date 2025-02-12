add78b9ef7f5b7bd23eb990a8f2018750be868d6
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-02-12 08:58:37 -0300

    Aula 9
    Pré incremento, pós incremento, inversão, string.

---
+++ aula09/aula09.js
@@ -0,0 +1,10 @@
+let n =- 10
+
+console.log(n++)
+console.log(--n)
+console.log(n1 + "" + n2)
+
+//Pós incremento fica na direita e não opera previamente
+//Pré incremento fica na esquerda e opera previamente
+//=- inverte o sinal do valor
+//Números podem ser juntados se somados junto com uma string 
