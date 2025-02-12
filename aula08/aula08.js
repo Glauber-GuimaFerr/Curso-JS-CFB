3552d219ff50285c61d0f007df6239909bd9dd62
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-02-12 08:41:20 -0300

    Aula 8
    Operadores bitwise.

---
+++ aula08/aula08.js
@@ -0,0 +1,13 @@
+/*
+& -> Equivalência se ambos foram iguais a 1
+| -> Equivalência se o 1 estiver presente
+^ -> Retorna 1 apenas se ambos foram diferentes  
+<< -> Dobra o valor n vezes deslocando os bits para esquerda
+>> -> Divide o valor n vezes deslocando os bits para direita
+*/
+
+let n1 = 25 //Binário
+let n2 = 11 //Binário
+
+let res = n1 << 2 //Binário para decimal
+console.log(res)
