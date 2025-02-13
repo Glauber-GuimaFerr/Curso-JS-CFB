759215db1023888caf971d5d9b4da083ec80d8f0
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-02-13 09:40:01 -0300

    Aula 15
    Loop for.

---
+++ aula15/aula15.js
@@ -0,0 +1,15 @@
+console.log('Início do programa')
+
+for(let i = 0; i < 10; i++){
+    console.log('CFB Cursos - valor do i: ' + i)    
+}
+
+console.log('Fim do programa')
+
+for(let i = 0; i < 100; i++){
+    if(i % 2 == 0){
+        console.log(i + ' é par')
+    }else{
+        console.log(i + ' é ímpar')
+    }
+}
