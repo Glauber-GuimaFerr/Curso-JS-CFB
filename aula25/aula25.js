a577d5c3931e942221a2ac032d0166410de64681
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-02-15 10:41:37 -0300

    Aula 25
    Arrow function.

---
+++ aula25/aula25.js
@@ -0,0 +1,17 @@
+const soma = (v1, v2) => {
+    let res = v1 + v2
+    return res
+}
+
+const nome = n => {return n}
+
+const add = n => n += 10 
+
+console.log(soma(10, 5))
+console.log(nome('Bruno'))
+console.log(add(10))
+
+//Arrow function é um tipo de função anônima com declaração simplificada
+//Os parênteses só são utilizados caso haja mais de um parâmetro 
+//As chaves podem ser substituídas por operações sem retorno
+//Utilize chaves para blocos de código
