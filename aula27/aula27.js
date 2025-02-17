0c43b003becd7756cca9da3efa691a935a946cca
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-02-17 09:15:19 -0300

    Aula 27
    Funções geradoras.

---
+++ aula27/aula27.js
@@ -0,0 +1,41 @@
+function* cores(){   
+    yield 'Vermelho'
+    yield 'Verde'
+    yield 'Azul'
+}
+
+const itc = cores()
+console.log(itc.next().value) //Vermelho
+console.log(itc.next().value) //Verde
+console.log(itc.next().value) //Azul
+console.log(itc.next().value) //Undefined
+
+function* perguntas(){
+    const nome = yield 'Qual seu nome?'
+    const esporte = yield 'Qual seu esporte favorito?'
+    return 'Seu nome é ' + nome + ', seu esporte favorito é ' + esporte
+}
+
+const itp = perguntas()
+console.log(itp.next().value) //Iniciação
+console.log(itp.next('Bruno').value)
+console.log(itp.next('Natação').value)
+
+function* contador(){
+    let i = 0
+    while(true){
+        yield i++
+        if(i > 5){
+            break
+        }
+    }
+}
+
+const itr = contador()
+
+for(let c of itr){
+    console.log(c)
+}
+
+//Funções geradoras são mais autônomas ao serem chamadas ou retornar valores
+//Elas possuem iteradores para execução de múltiplas tarefas
