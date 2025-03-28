112938dd6755cd7ac6c434bcd9c6b56edc7bbf9f
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-03-28 08:22:40 -0300

    Aula 78
    Sexta atividade, se tratando de um relógio que se atualiza a cada segundo.

---
+++ aula78_A6/aula78.js
@@ -0,0 +1,28 @@
+const div_data = document.getElementById('div_data')
+const data = new Date()
+
+let dia = data.getDate()
+dia = dia < 10 ? '0' + dia : dia
+let mes = data.getMonth()
+mes = mes < 10 ? '0' + mes : mes
+const data_r = dia + '/' + mes + '/' + data.getFullYear()
+div_data.innerHTML = data_r
+
+const div_relogio = document.getElementById('div_relogio')
+
+const relogio = () => {
+    const data = new Date()
+    let hora = data.getHours()
+    hora = hora < 10 ? '0' + hora:hora
+    let minuto = data.getMinutes()
+    minuto = minuto < 10 ? '0' + minuto:minuto
+    let segundo = data.getSeconds()
+    segundo = segundo < 10 ? '0' + segundo:segundo
+    const hora_completa = hora + ':' + minuto + ':' + segundo
+    div_relogio.innerHTML = hora_completa
+}
+
+const intervalo = setInterval(relogio, 1000)
+
+//setInterval() define um intervalo para determinada função ser executada
+//Parênteses deve ser observado
