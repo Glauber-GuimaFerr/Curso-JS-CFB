af2ca6fa28df1135d1971ae743add0fa7c49d7d2
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-04-03 14:39:15 -0300

    Aula 92
    Módulos.

---
+++ aula92/aula92.js
@@ -0,0 +1,10 @@
+import { cursos } from "./curso.js"
+import {carros} from "./curso.js"
+
+console.log('servidor rodando')
+console.log(cursos)
+console.log(carros)
+
+//Módulos são uma forma de trabalhar com arquivos separados por diretórios, permitindo o reaproveitamento de código
+//export {} indica o que será exportado do script
+//import {} from importa o conteúdo indicado pelo caminho do diretório 
