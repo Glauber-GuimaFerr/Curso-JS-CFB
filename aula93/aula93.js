e1fe4967ca3299b8f62550cf4076fe1a841faf4f
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-04-03 14:53:48 -0300

    Aula 93
    Export default.

---
+++ aula93/aula93.js
@@ -0,0 +1,8 @@
+import getTodosCursos, {cursos, getCursos} from './curso2.js'
+
+console.log(cursos)
+console.log(getTodosCursos())
+console.log(getCursos(0))
+
+//O script do HTML deverá apresentar o atributo type = "module"
+//export default não exige chaves e só pode ser utilizado uma vez por script
