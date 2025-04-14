1fd064451950f901e66e50544a95cbad1b9b57b7
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-04-14 15:05:10 -0300

    Aula 118
    Uso do ponto e vírgula.

---
+++ aula118/aula118.js
@@ -0,0 +1,27 @@
+let i = null; i = 0
+console.log(i)
+
+let canal = 'CFB Cursos';
+['J', 'a', 'v', 'a', 'S', 'c', 'r', 'i', 'p', 't'].forEach(l => {console.log(l)}); 
+
+let n1 = [1, 2]
+let n2 = [3, 4];
+[n1, n2].map(v => console.log(v))
+
+let n = null;
+(n = 10)
+console.log(n)
+
+let num = {
+    n1: 10,
+    n2: 20,
+    n3: 30
+}
+let {nu1, nu2, nu3} = num
+console.log(nu1)
+console.log(nu2)
+console.log(nu3)
+
+//Quebra de linha será interpretada como ponto e vírgula
+//Linhas vazias são desconsideradas
+//Se a linha seguinte possuir um colchete ou parênteses no início, o ponto e vírgula é obrigatório
