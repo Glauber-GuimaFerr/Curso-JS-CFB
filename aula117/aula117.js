b35b026dc20ffcaaeffd8c43c9594b9601e4628d
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-04-14 09:28:37 -0300

    Aula 117
    Documentação ECMA do ponto e vírgula.

---
+++ aula117/aula117.js
@@ -0,0 +1,6 @@
+let i = null
+for(i = 0; i < 10; i++);
+console.log(i)
+
+//Segundo o manual da ECMA, o interpretador JavaScript faz a inserção automática do ponto e vírgula
+//Em loopings sem blocos de comando o ';' é obrigatório
