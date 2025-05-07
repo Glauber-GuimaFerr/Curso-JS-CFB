1caebd1584300e5e0c7ede7969c812399fc1ef7f
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-05-07 08:46:16 -0300

    Aula 167
    Parte 8 da atividade 17: Adição do CSS dos campos de pesquisa e iniciação do script.

---
+++ aula167_A17/js/pesquisar.js
@@ -0,0 +1,11 @@
+const btn_pesq = document.querySelector('#btn_pesq')
+const f_txtpesq = document.querySelector('#f_txtpesq')
+
+btn_pesq.addEventListener('click', (evt) => {
+    if(f_txtpesq.value == ''){
+        alert('Digite a pesquisa!')
+        f_txtpesq.focus()
+        return
+    }
+    document.getElementsByName('f_pesq').value
+})
