804bdaf49f8e4d28ad41dcf3603575adca80d0f8
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-05-23 14:53:11 -0300

    Aula 189
    Parte 9 da atividade 18: Criação do popup de novo colaborador.

---
+++ aula189_A18/src/server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'estoque1_js'
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/todosusuarios', (req, res) => { 
+  const query = 'SELECT * FROM usuario';
+
+  connection.query(query, (error, results) => {
+    if(error){
+      console.error('Erro ao executar a query:', error);
+      return res.status(500).send('Erro no servidor');
+    }
+    res.json(results);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
