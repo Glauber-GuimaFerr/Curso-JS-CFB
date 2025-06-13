ae619e959acc21ae253aec62241ee338f3f63bb2
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-06-13 09:36:30 -0300

    Aula 221
    Parte 41 da atividade 18: Ajustes no botão de status de fornecedor e atualização no botão de adicionar.

---
+++ aula221_A18/src/backoffice/fornecedores/fornecedores.js
@@ -0,0 +1,303 @@
+import {Cxmsg} from '../../utils/cxmsg.js'
+
+/* IDs */
+const dadosGrid = document.querySelector('#dadosGrid')
+const btn_add = document.querySelector('#btn_add')
+const novoFornecedor = document.querySelector('#novoFornecedor')
+const btn_fecharPopup = document.querySelector('#btn_fecharPopup')
+const btn_fecharPopupPesq = document.querySelector('#btn_fecharPopupPesq')
+const btn_gravarPopup = document.querySelector('#btn_gravarPopup')
+const btn_cancelarPopup = document.querySelector('#btn_cancelarPopup')
+const f_nome = document.querySelector('#f_nome')
+const f_status = document.querySelector('#f_status')
+const f_foto = document.querySelector('#f_foto')
+const img_foto = document.querySelector('#img_foto')
+const f_filtragem = document.querySelector('#f_filtragem')
+const pesquisa = document.querySelector('#pesquisa')
+const btn_pesq = document.querySelector('#btn_pesq')
+const f_pesqId = document.querySelector('#f_pesqId')
+const f_pesqNome = document.querySelector('#f_pesqNome')
+const f_pesq = document.querySelector('#f_pesq')
+const btn_pesquisar = document.querySelector('#btn_pesquisar')
+const btn_listarTudo = document.querySelector('#btn_listarTudo')
+
+/* Variáveis importantes */
+let modojanela = 'n' //n = Novo fornecedor | e = Editar novo fornecedor
+const sv = sessionStorage.getItem('servidor_node') 
+
+/* Funções de filtragem */
+f_filtragem.addEventListener('keyup', (evt) => {
+    const linhas = [...document.querySelectorAll('.linhaGrid')]
+    let input, texto, filtragem
+    input = evt.target
+    filtragem = input.value.toUpperCase()
+    for(let i = 0; i < linhas.length; i++){
+        texto = linhas[i].children[1].innerHTML
+        if(texto.toUpperCase().indexOf(filtragem) > -1){
+            document.getElementById('tituloPopupPesquisar').innerHTML = 'Pesquisar Fornecedor'
+            linhas[i].classList.remove('ocultarLinhaGrid')
+        }else{
+            linhas[i].classList.add('ocultarLinhaGrid')
+        }
+    }
+})
+
+btn_fecharPopupPesq.addEventListener('click', (evt) => {
+    pesquisa.classList.add('ocultarPopup')
+})
+
+btn_pesq.addEventListener('click', (evt) => {
+    pesquisa.classList.remove('ocultarPopup')
+    f_pesq.value = ''
+    f_pesq.focus()
+})
+
+f_pesqId.addEventListener('click', (evt) => {
+    f_pesq.value = ''
+    f_pesq.focus()
+})
+
+f_pesqNome.addEventListener('click', (evt) => {
+    f_pesq.value = ''
+    f_pesq.focus()
+})
+
+btn_pesquisar.addEventListener('click', (evt) => {
+    let tipo = null
+    if(f_pesqId.checked){
+        tipo = 'id'
+    }else{
+        tipo = 'nome'
+    }
+    
+    if(f_pesq.value != ''){
+        const endpointpesq = `${sv}/pesquisafornecedor/${tipo}/${f_pesq.value}`
+        fetch(endpointpesq)
+        .then(res => res.json())
+        .then(res => {
+            dadosGrid.innerHTML = ''
+            res.forEach(e => {
+                criarLinha(e)
+            })
+        })
+        pesquisa.classList.add('ocultarPopup')
+    }else{
+        const config = {
+            titulo: "Alerta",
+            texto: "Preencha o campo de pesquisa! Digite o nome ou ID do fornecedor.",
+            cor: "#008",
+            tipo: "ok",
+            ok: () => {},
+            sim: () => {},
+            nao: () => {}
+        }
+        Cxmsg.mostrar(config)
+    }
+})
+
+btn_listarTudo.addEventListener('click', (evt) => {
+    carregarTodosFornecedores()
+})
+
+/* Funções para carregar a tabela */
+const carregarTodosFornecedores = () => {
+    const endpoint = `${sv}/todosfornecedores`
+    fetch(endpoint)
+    .then(res => res.json())
+    .then(res => {
+        dadosGrid.innerHTML = ''
+        res.forEach(e => {
+            criarLinha(e)
+        })
+    })
+}
+carregarTodosFornecedores()
+
+const criarLinha = (e) => {
+    const divLinha = document.createElement('div')
+    divLinha.setAttribute('class', 'linhaGrid')
+    
+    const divc1 = document.createElement('div')
+    divc1.setAttribute('class', 'colunaLinhaGrid c1')
+    divc1.innerHTML = e.n_fornecedor_fornecedor
+    divLinha.appendChild(divc1)
+
+    const divc2 = document.createElement('div')
+    divc2.setAttribute('class', 'colunaLinhaGrid c2')
+    divc2.innerHTML = e.s_desc_fornecedor
+    divLinha.appendChild(divc2)
+
+    const divc3 = document.createElement('div')
+    divc3.setAttribute('class', 'colunaLinhaGrid c3')
+    divc3.innerHTML = e.c_status_fornecedor
+    divLinha.appendChild(divc3)
+
+    const divc4 = document.createElement('div')
+    divc4.setAttribute('class', 'colunaLinhaGrid c4')
+    divLinha.appendChild(divc4)
+    
+    const img_status = document.createElement('img')
+    if(e.c_status_fornecedor == 'A'){
+        img_status.setAttribute('src', '../../shared/img/on.svg')
+    }else{
+        img_status.setAttribute('src', '../../shared/img/off.svg')
+    }
+    img_status.setAttribute('data-idfornecedor', e.n_fornecedor_fornecedor)
+    img_status.setAttribute('class', 'icone_op')
+    img_status.addEventListener('click', (evt) => {
+        const idfornecedor = evt.target.dataset.idfornecedor
+        if(evt.target.getAttribute('src') == '../../shared/img/on.svg'){
+            const endpoint_mudarStatus = `${sv}/mudarstatusfornecedor/${idfornecedor}/I`
+            fetch(endpoint_mudarStatus)
+            .then(res => {
+                if(res.status == 200){
+                    evt.target.setAttribute('src', '../../shared/img/off.svg')
+                    evt.target.parentNode.parentNode.childNodes[2].innerHTML = 'I'
+                }
+            })
+        }else{
+            const endpoint_mudarStatus = `${sv}/mudarstatusfornecedor/${idfornecedor}/A`
+            fetch(endpoint_mudarStatus)
+            .then(res => {
+                if(res.status == 200){
+                    evt.target.setAttribute('src', '../../shared/img/on.svg')
+                    evt.target.parentNode.parentNode.childNodes[2].innerHTML = 'A'
+                }
+            })
+        }
+    })
+    divc4.appendChild(img_status)
+
+    const img_editar = document.createElement('img')
+    img_editar.setAttribute('src', '../../shared/img/editar.svg')
+    img_editar.setAttribute('class', 'icone_op')
+    img_editar.addEventListener('click', (evt) => {
+        const id = evt.target.parentNode.parentNode.firstChild.innerHTML
+        modojanela = 'e'
+        document.getElementById('tituloPopup').innerHTML = 'Editar Fornecedor'
+        const endpoint = `${sv}/dadosfornecedor/${id}`
+        fetch(endpoint)
+        .then(res => res.json())
+        .then(res => {
+            btn_gravarPopup.setAttribute('data-idfornecedor', id)
+            f_nome.value = res[0].s_desc_fornecedor
+            f_status.value = res[0].c_status_fornecedor
+            img_foto.src = res[0].s_logo_fornecedor
+            novoFornecedor.classList.remove('ocultarPopup')
+            if(img_foto.src == '' || img_foto.src == '#'){
+                img_foto.classList.add('esconderElemento')
+            }else{
+                img_foto.classList.remove('esconderElemento')
+            }
+        })
+    })
+    divc4.appendChild(img_editar)
+
+    dadosGrid.appendChild(divLinha)
+}
+
+/* Eventos de botões internos e externos do pop-up principal e conexão de APIs */
+btn_add.addEventListener('click', (evt) => {
+    modojanela = 'n'
+    document.getElementById('tituloPopup').innerHTML = 'Novo Fornecedor'
+    novoFornecedor.classList.remove('ocultarPopup')
+    img_foto.classList.add('esconderElemento')
+    f_nome.value = ''
+    f_status.value = ''
+    f_foto.value = ''
+    img_foto.setAttribute('src', '#')
+})
+
+btn_fecharPopup.addEventListener('click', (evt) => {
+    novoFornecedor.classList.add('ocultarPopup')
+})
+
+btn_gravarPopup.addEventListener('click', (evt) => {
+    const dados = {
+        n_fornecedor_fornecedor: evt.target.dataset.idfornecedor,
+        s_desc_fornecedor: f_nome.value,
+        s_logo_fornecedor: img_foto.getAttribute("src"),
+        c_status_fornecedor: f_status.value,
+    }
+
+    const cabecalho = {
+        method: modojanela == "n" ? "POST" : "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dados)
+    }
+
+    let endpoint_novoXeditarFornec = null
+    if(modojanela == 'n'){
+        endpoint_novoXeditarFornec = `${sv}/novofornecedor` 
+    }else{
+        endpoint_novoXeditarFornec = `${sv}/editarfornecedor/${evt.target.dataset.idfornecedor}`
+    }
+    fetch(endpoint_novoXeditarFornec, cabecalho)
+    .then(res => {
+        if(res.status == 200){
+            if(modojanela == 'n'){
+                const config = {
+                    titulo: "OK",
+                    texto: "Novo fornecedor gravado!",
+                    cor: "#008",
+                    tipo: "ok",
+                    ok: () => {},
+                    sim: () => {},
+                    nao: () => {}
+                }
+                Cxmsg.mostrar(config) 
+                f_nome.value = ''
+                f_status.value = ''
+                f_foto.value = ''
+                img_foto.setAttribute('src', '#')
+            }else{
+                const config = {
+                    titulo: "OK",
+                    texto: "Alterações de fornecedor salvas!",
+                    cor: "#008",
+                    tipo: "ok",
+                    ok: () => {},
+                    sim: () => {},
+                    nao: () => {}
+                }
+                Cxmsg.mostrar(config)
+            }
+            carregarTodosFornecedores()
+        }else{
+            const config = {
+                titulo: "Erro",
+                texto: "Erro ao gravar dados.",
+                cor: "#f00",
+                tipo: "ok",
+                ok: () => {},
+                sim: () => {},
+                nao: () => {}
+            }
+            Cxmsg.mostrar(config)
+        }
+    }).finally(() => {
+        img_foto.classList.add('esconderElemento')
+    })
+})
+
+btn_cancelarPopup.addEventListener('click', (evt) => {
+    novoFornecedor.classList.add('ocultarPopup')
+})
+
+/* Funções para upload de imagens na base 64 */
+const converter_imagem_b64 = (localDestino, arquivoImg) => {
+    const obj = arquivoImg
+    const reader = new FileReader()
+    reader.addEventListener('load', (evt) => {
+        localDestino.src = reader.result
+    })
+    if(obj){
+        reader.readAsDataURL(obj)
+    }
+}
+
+f_foto.addEventListener('change', (evt) => {
+    converter_imagem_b64(img_foto, evt.target.files[0]);
+})
---
+++ aula221_A18/src/server/server.js
@@ -0,0 +1,246 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'estoque1_js'
+});
+
+app.use(cors());
+app.use(express.json());
+
+//Estoque - Colaboradores
+app.get('/todaspessoas', (req, res) => { 
+  const query = 'SELECT * FROM pessoa';
+
+  connection.query(query, (error, results) => {
+    if(error){
+      console.error('Erro ao executar a query:', error);
+      return res.status(500).send('Erro no servidor');
+    }
+    res.json(results);
+  });
+});
+
+app.get('/tiposcolab', (req, res) => { 
+  const query = 'SELECT * FROM tipopessoa';
+
+  connection.query(query, (error, results) => {
+    if(error){
+      console.error('Erro ao executar a query:', error);
+      return res.status(500).send('Erro ao recuperar tipos de colaborador');
+    }
+    res.json(results);
+  });
+});
+
+app.post('/novocolab', (req, res) => {
+  const { s_nome_pessoa, n_tipopessoa_pessoa, c_status_pessoa, s_foto_pessoa, numtelefones } = req.body;
+  const query1 = `INSERT INTO pessoa (n_pessoa_pessoa, s_nome_pessoa, n_tipopessoa_pessoa, c_status_pessoa, s_foto_pessoa) VALUES (NULL, ?, ?, ?, ?)`;
+  const values = [s_nome_pessoa, n_tipopessoa_pessoa, c_status_pessoa, s_foto_pessoa];
+
+  connection.query(query1, values, (error, results) => {
+    if(error){
+      console.error('Erro ao executar a query:', error);
+      return res.status(500).send('Erro ao inserir dados no servidor');
+    }
+
+    const pessoaId = results.insertId;
+    if(Array.isArray(numtelefones) && numtelefones.length > 0){
+      const telefonesValues = numtelefones.map(tel => [null, pessoaId, tel]);
+      const query2 = `INSERT INTO telefone (n_telefone_telefone, n_pessoa_telefone, s_numero_telefone) VALUES ?`;
+
+      connection.query(query2, [telefonesValues], (error2, results2) => {
+        if(error2){
+          console.error('Erro ao executar a query:', error2);
+          return res.status(500).send('Erro ao inserir telefones');
+        }
+        res.json(results2);
+      });
+    }else{
+      res.status(200).json({ mensagem: "Novo colaborador gravado!" });
+    }
+  });
+});
+
+app.get('/dadoscolab/:id', (req, res) => { 
+  const parametro = req.params.id;
+  const query = 'SELECT * FROM pessoa WHERE n_pessoa_pessoa = ?';
+
+  connection.query(query, [parametro], (error, results) => {
+    if(error){
+      console.error('Erro ao executar a query:', error);
+      return res.status(500).send('Erro ao recuperar colaborador');
+    }
+    res.json(results);
+  });
+});
+
+app.get('/telefonescolab/:id', (req, res) => { 
+  const parametro = req.params.id;
+  const query = 'SELECT * FROM telefone WHERE n_pessoa_telefone = ?';
+
+  connection.query(query, [parametro], (error, results) => {
+    if(error){
+      console.error('Erro ao executar a query:', error);
+      return res.status(500).send('Erro ao recuperar telefones do colaborador');
+    }
+    res.json(results);
+  });
+});
+
+app.put('/editarcolab/:id', (req, res) => {
+  const { s_nome_pessoa, n_tipopessoa_pessoa, c_status_pessoa, s_foto_pessoa, numtelefones } = req.body;
+  const id = req.params.id;
+  const query1 = `UPDATE pessoa SET s_nome_pessoa = ?, n_tipopessoa_pessoa = ?, c_status_pessoa = ?, s_foto_pessoa = ? WHERE n_pessoa_pessoa = ?`;
+  const values = [s_nome_pessoa, n_tipopessoa_pessoa, c_status_pessoa, s_foto_pessoa, id];
+
+  connection.query(query1, values, (error, results) => {
+    if(error){
+      console.error('Erro ao executar a query:', error);
+      return res.status(500).send('Erro ao atualizar dados no servidor');
+    }
+
+    if(Array.isArray(numtelefones) && numtelefones.length > 0){
+      const telefonesValues = numtelefones.map(tel => [null, id, tel]);
+      const query2 = `
+        INSERT INTO telefone (n_telefone_telefone, n_pessoa_telefone, s_numero_telefone) VALUES ?`;
+
+      connection.query(query2, [telefonesValues], (error2, results2) => {
+        if(error2){
+          console.error('Erro ao inserir novos telefones:', error2);
+          return res.status(500).send('Erro ao inserir novos telefones');
+        }
+        res.json(results2);
+      });
+    }else{
+      return res.status(200).json({ mensagem: "Colaborador atualizado!" });
+    }
+  });
+});
+
+app.get('/deletartelefone/:id', (req, res) => { 
+  const parametro = req.params.id;
+  const query = 'DELETE FROM telefone WHERE n_telefone_telefone = ?';
+
+  connection.query(query, [parametro], (error, results) => {
+    if(error){
+      console.error('Erro ao executar a query:', error);
+      return res.status(500).send('Erro ao excluir telefone');
+    }
+    res.json(results);
+  });
+});
+
+app.get('/mudarstatuscolab/:id/:status', (req, res) => { 
+  const id = req.params.id;
+  const status = req.params.status;
+  const query = 'UPDATE pessoa SET c_status_pessoa = ? WHERE n_pessoa_pessoa = ?';
+
+  connection.query(query, [status, id], (error, results) => {
+    if(error){
+      console.error('Erro ao executar a query:', error);
+      return res.status(500).send('Erro ao alterar status do colaborador');
+    }
+    res.json(results);
+  });
+});
+
+app.get('/deletarcolab/:id', (req, res) => { 
+  const parametro = req.params.id;
+  const query = 'DELETE FROM pessoa WHERE n_pessoa_pessoa = ?';
+
+  connection.query(query, [parametro], (error, results) => {
+    if(error){
+      console.error('Erro ao executar a query:', error);
+      return res.status(500).send('Erro ao excluir colaborador');
+    }
+    res.json(results);
+  });
+});
+
+app.get('/pesquisacolab/:tipo/:valor', (req, res) => { 
+  const tipo = req.params.tipo;
+  let valor = req.params.valor;
+  let query = '';
+  if(tipo == 'id'){
+    query = 'SELECT * FROM pessoa WHERE n_pessoa_pessoa = ?';    
+  }else{
+    valor = `%${req.params.valor}%`;
+    query = 'SELECT * FROM pessoa WHERE s_nome_pessoa LIKE ?';
+  }
+
+  connection.query(query, [valor], (error, results) => {
+    if(error){
+      console.error('Erro ao executar a query:', error);
+      return res.status(500).send('Erro ao recuperar colaborador');
+    }
+    res.json(results);
+  });
+});
+
+//Estoque - Fornecedores
+app.get('/todosfornecedores', (req, res) => { 
+  const query = 'SELECT * FROM fornecedor';
+
+  connection.query(query, (error, results) => {
+    if(error){
+      console.error('Erro ao executar a query:', error);
+      return res.status(500).send('Erro no servidor');
+    }
+    res.json(results);
+  });
+});
+
+app.get('/mudarstatusfornecedor/:id/:status', (req, res) => { 
+  const id = req.params.id;
+  const status = req.params.status;
+  const query = 'UPDATE fornecedor SET c_status_fornecedor = ? WHERE n_fornecedor_fornecedor = ?';
+
+  connection.query(query, [status, id], (error, results) => {
+    if(error){
+      console.error('Erro ao executar a query:', error);
+      return res.status(500).send('Erro ao alterar status do fornecedor');
+    }
+    res.json(results);
+  });
+});
+
+app.post('/novofornecedor', (req, res) => {
+  const { s_nome_pessoa, n_tipopessoa_pessoa, c_status_pessoa, s_foto_pessoa, numtelefones } = req.body;
+  const query1 = `INSERT INTO pessoa (n_pessoa_pessoa, s_nome_pessoa, n_tipopessoa_pessoa, c_status_pessoa, s_foto_pessoa) VALUES (NULL, ?, ?, ?, ?)`;
+  const values = [s_nome_pessoa, n_tipopessoa_pessoa, c_status_pessoa, s_foto_pessoa];
+
+  connection.query(query1, values, (error, results) => {
+    if(error){
+      console.error('Erro ao executar a query:', error);
+      return res.status(500).send('Erro ao inserir dados no servidor');
+    }
+
+    const pessoaId = results.insertId;
+    if(Array.isArray(numtelefones) && numtelefones.length > 0){
+      const telefonesValues = numtelefones.map(tel => [null, pessoaId, tel]);
+      const query2 = `INSERT INTO telefone (n_telefone_telefone, n_pessoa_telefone, s_numero_telefone) VALUES ?`;
+
+      connection.query(query2, [telefonesValues], (error2, results2) => {
+        if(error2){
+          console.error('Erro ao executar a query:', error2);
+          return res.status(500).send('Erro ao inserir telefones');
+        }
+        res.json(results2);
+      });
+    }else{
+      res.status(200).json({ mensagem: "Novo colaborador gravado!" });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
