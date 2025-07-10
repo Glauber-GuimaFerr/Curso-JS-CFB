e77c6e4c46c23083f2d9339777a489884148471c
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-07-10 09:36:08 -0300

    Aula 250
    Parte 70 da atividade 18: Finalização da verificação de status da api de login.

---
+++ aula250_A18/src/backoffice/login/index.js
@@ -0,0 +1,47 @@
+import {Cxmsg} from '../../utils/cxmsg.js'
+
+const f_email = document.querySelector('#f_email')
+const f_senha = document.querySelector('#f_senha')
+const btn_login = document.querySelector('#btn_login')
+
+let sv = null
+
+const endpoint_config = '../../../config.txt'
+fetch(endpoint_config)
+.then(res => res.json())
+.then(res => {
+    sessionStorage.setItem('servidor_node', res.servidor_node)
+    sessionStorage.setItem('versao', res.versao)
+    sv = res.servidor_node
+})
+
+btn_login.addEventListener('click', (evt) => {
+    if(sv != null){
+        if(f_email.value != '' && f_senha.value != ''){
+            const email = f_email.value
+            const senha = f_senha.value
+            const endpoint = `${sv}/login/${email}/${senha}`
+            fetch(endpoint)
+            .then(res => {
+                if(res.status == 200){
+
+                }else if(res.status == 205){
+ 
+                }else if(res.status == 208){
+                    const config = {
+                        titulo: "Erro",
+                        texto: "Usuário ou senha inválidos!",
+                        cor: "#f00",
+                        tipo: "ok",
+                        ok: () => {},
+                        sim: () => {},
+                        nao: () => {}
+                    }
+                    Cxmsg.mostrar(config)
+                }
+            })
+        }else{
+            alert('Preencha os campos vazios.')
+        }
+    }
+})
