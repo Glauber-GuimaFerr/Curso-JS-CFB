c11a533ea6edb2f5b79fda14d72826ddd749e158
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-07-14 09:22:10 -0300

    Aula 258
    Parte 78 da atividade 18: Finalização da api de entrada de token e adição da permissão de exclusão para administradores.

---
+++ aula258_A18/src/backoffice/principal/main.js
@@ -0,0 +1,31 @@
+if(sessionStorage.getItem('n_pessoa_pessoa') == '-1'){
+    window.location.href = '../login/index.html'
+}
+
+const btn_menuPrincipal = document.querySelector('#btn_menuPrincipal')
+const menuPrincipal = document.querySelector('#menuPrincipal')
+const todosmenusprincipais = [...document.querySelectorAll('.btn_menuItem')]
+const divid = document.querySelector('#divid')
+const divnome = document.querySelector('#divnome')
+const btn_logoff = document.querySelector('#btn_logoff')
+
+btn_menuPrincipal.addEventListener('click', (evt) => {
+    menuPrincipal.classList.toggle('ocultar')
+})
+
+todosmenusprincipais.forEach(e => {
+    e.addEventListener('click', (evt) => {
+        menuPrincipal.classList.add('ocultar')
+    })
+})
+
+btn_logoff.addEventListener('click', (evt) => {
+    sessionStorage.setItem('n_pessoa_pessoa', '-1')
+    sessionStorage.setItem('s_nome_pessoa', '-1')
+    window.location.href = '../login/index.html'
+})
+
+const n_pessoa_pessoa = sessionStorage.getItem('n_pessoa_pessoa')
+const s_nome_pessoa = sessionStorage.getItem('s_nome_pessoa')
+divid.innerHTML = `id: ${n_pessoa_pessoa}`
+divnome.innerHTML = `nome: ${s_nome_pessoa}` 
